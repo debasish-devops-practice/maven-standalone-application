ecc569755794463c19dc09f91d6b71495e831dc7
Devsam180 <60935093+Devsam180@users.noreply.github.com>
2022-04-11 18:44:19 +0530

    Create Webconfig.java

---
+++ src/main/java/com/Webconfig.java
@@ -0,0 +1,16 @@
+package com;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import javax.servlet.Filter;
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
+import com.amazonaws.xray.strategy.SegmentNamingStrategy;
+
+@Configuration
+public class WebConfig {
+
+  @Bean
+  public Filter TracingFilter() {
+    return new AWSXRayServletFilter(SegmentNamingStrategy.dynamic("com", "*.example.com"));
+  }
+}
+
