5605c35d47ba6d06cb39d23362d220329b31a8a4
Devsam180 <60935093+Devsam180@users.noreply.github.com>
2022-04-11 18:27:42 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -6,21 +6,6 @@ package com.mt.sample;
  */
 import java.util.Date;
 
-package sample;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import javax.servlet.Filter;
-import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import com.amazonaws.xray.strategy.SegmentNamingStrategy;
-
-@Configuration
-public class WebConfig {
-
-  @Bean
-  public Filter TracingFilter() {
-    return new AWSXRayServletFilter(SegmentNamingStrategy.dynamic("sample", "maven.exaple.com"));
-  }
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
